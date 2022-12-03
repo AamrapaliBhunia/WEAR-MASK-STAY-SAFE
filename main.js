a69f6542ea7aac3b2ed64e5aa8ca9707e438ab15
AamrapaliBhunia <101936608+AamrapaliBhunia@users.noreply.github.com>
2022-12-03 16:49:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+prediction_1 = ""
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/o7tX66pjy/model.json',modelLoaded);
+
+//define function modelLoaded
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+
+}
+//define function check() 
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+
+}
+
+
+//define function gotResult(error, results)
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("status").innerHTML = results[0].label;
+        
+        prediction_1 = results[0].label;
+    
+        if(results[0].label == "Yes")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#x1F637;";
+
+        }
+        if(results[0].label == "No")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#x26d4;";
+
+        }
+        if(results[0].label == "Good")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#x1F637;";
+
+        }
+
+        if(results[0].label == "Bad")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#x26d4;";
+
+        }
+
+        if(results[0].label == "Ok")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#x1F637;";
+
+        }
+        
+    }
+}
+
+
